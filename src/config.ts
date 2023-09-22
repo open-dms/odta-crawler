5b8d3dd4b57b000f09f0f200e618c05c6d083292
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2023-09-22 13:35:21 -0300

    feat: add mongodb write stream

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 import "dotenv/config";
 
-const { API_URL, API_KEY, LOG_LEVEL } = process.env;
+const { API_URL, API_KEY, LOG_LEVEL, MONGO_URL, MONGO_CERT_FILE, DB_NAME } =
+  process.env;
 
 export const apiUrl = API_URL || "";
 export const apiKey = API_KEY || "";
 export const level = LOG_LEVEL || "info";
+export const mongoUrl = MONGO_URL || "";
+export const mongoCertFile = MONGO_CERT_FILE || "";
+export const dbName = DB_NAME || "";
